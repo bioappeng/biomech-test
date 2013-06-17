a227e2df51dd728a5afc5e4cd2959aeb532e3e9a
John Peterson <sn0risn@gmail.com>
2013-06-17 09:43:25 -0400

    dropSet basics
    finished a constructor -- all the basic data reads in and is *there*.
    possibly should do some of that with helper methods from a maintenance
    standpoint.

--- dropSet.m
+++ dropSet.m
@@ -1,5 +1,6 @@
 classdef dropSet
     properties
+        isThreeAxLoad
         pos
         load
         accx
@@ -10,22 +11,41 @@ classdef dropSet
         loadz
     end
     methods
-        function obj = dropSet(pos, load, accx, accy, accz)
-            obj.pos = pos;
-            obj.load = load;
-            obj.accx = accx;
-            obj.accy = accy;
-            obj.accz = accz;
-        end
-        function obj = dropSet_3axLoad(pos, accx, accy, accz, loadx, loady, loadz)
-            obj.pos = pos;
-            obj.load = load;
-            obj.accx = accx;
-            obj.accy = accy;
-            obj.accz = accz;
-            obj.loadx = loadx;
-            obj.loady = loady;
-            obj.loadz = loadz;
+        function obj = dropSet(path, num_headerlines, three_axis_load)
+            fext = '*.txt';
+            flist = dir([path, fext]);
+            numfiles = size(flist,1);
+            
+            obj.isThreeAxLoad = three_axis_load;
+            
+            if three_axis_load == 0
+                numfields = '%f%f%f%f%f'; %string pot, single ax load, triax accel
+                num_cols = 5;
+            elseif three_axis_load == 1
+                numfields = '%f%f%f%f%f%f%f%f'; %If triaxial load cell included
+                num_cols = 8;
+            end
+            
+            data = cell(numfiles, num_cols);
+            
+            for i=1:numfiles
+                filepath = [path, flist(i,1).name];
+                file = fopen(filepath);
+                data(i,:) = textscan(file, numfields, 'HeaderLines', num_headerlines);
+                fclose(file);
+            end
+            
+            obj.pos = data{i,1}(:,1);    
+            obj.load = data{i,2}(:,1);
+            obj.accx = data{i,8}(:,1);    
+            obj.accy = data{i,6}(:,1);    
+            obj.accz = data{i,7}(:,1);
+            
+            if obj.isThreeAxLoad == 1;
+                obj.loadx = data{i,4}(:,1);
+                obj.loady = data{i,3}(:,1);
+                obj.loadz = data{i,5}(:,1);
+            end
         end
     end
 end

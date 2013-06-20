b46eb4688ee2100981c391c471ee21b2d1c7dd34
John Peterson <sn0risn@gmail.com>
2013-06-20 13:16:24 -0400

    resolved a bit of mess from earlier cherry-picking

--- basic_gui.m
+++ basic_gui.m
@@ -4,16 +4,23 @@ function basic_gui
               'Position',[360,500,450,300],...
               'MenuBar','none',...
               'Name','files selection');
-    filepath = uicontrol('Style', 'pushbutton', 'String', 'path to datafiles',...
-                        'Position', [25,260,100,25], 'Callback',{@filepath_Callback});
-    headerlines = uicontrol('Style', 'edit', 'String', '# header lines',...
-                            'Position', [25,230,80,20], 'Callback',{@headerlines_Callback});
+          
+    %ui elements
+    filepath = uicontrol('Style', 'pushbutton',...
+                         'String', 'path to datafiles',...
+                        'Position', [25,260,100,25],...
+                        'Callback',{@filepath_Callback});
+    headerlines = uicontrol('Style', 'edit',...
+                            'String', '# header lines',...
+                            'Position', [25,230,80,20],...
+                            'Callback',{@headerlines_Callback});
     filelist = uicontrol('Style', 'listbox',...
                         'Position', [25,45,400,160]);
     submit_button = uicontrol('Style', 'pushbutton', 'String', 'Done',...
-                            'Position', [390,10,50,25],'Callback',{@submit_button_Callback});
+                            'Position', [390,10,50,25],...
+                            'Callback',{@submit_button_Callback});
 
-
+    %ui element callbacks
     function filepath_Callback(source,eventdata)
         dropSet_filepath = [uigetdir(pwd), '\'];
     end
@@ -26,10 +33,12 @@ function basic_gui
 %         index_selected = get(filelist, 'Value');
 %         filelist_all = get(filelist, 'String');
 %         filelist_selected = filelist_all(index_selected); %this probably won't work
-        test = dropSet('test', dropSet_filepath, dropSet_headerlines, true)
+        test = dropSet(dropSet_filepath, dropSet_headerlines, true)
     end
 
-    dropSet_filepath = [];
+    %initialize dropSet data (defaults)
+    %these should be read from settings file in release versions
+    dropSet_filepath = [pwd, '\'];
     dropSet_headerlines = 0;
 
     set(f, 'Visible', 'on');

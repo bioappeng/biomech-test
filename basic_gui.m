7d9481bba24e20c4b6a50f57a173866a7e5fd7f3
John Peterson <sn0risn@gmail.com>
2013-06-20 10:45:59 -0400

    started work on simple gui
    in development. not using the header_lines callback or anything related to
    the listbox right now. going to have to figure out how to update that to
    get thie file list from the directory.

---
+++ basic_gui.m
@@ -0,0 +1,36 @@
+function basic_gui
+
+    f = figure('Visible','off',...
+              'Position',[360,500,450,300],...
+              'MenuBar','none',...
+              'Name','files selection');
+    filepath = uicontrol('Style', 'pushbutton', 'String', 'path to datafiles',...
+                        'Position', [25,260,100,25], 'Callback',{@filepath_Callback});
+    headerlines = uicontrol('Style', 'edit', 'String', '# header lines',...
+                            'Position', [25,230,80,20], 'Callback',{@headerlines_Callback});
+    filelist = uicontrol('Style', 'listbox',...
+                        'Position', [25,45,400,160]);
+    submit_button = uicontrol('Style', 'pushbutton', 'String', 'Done',...
+                            'Position', [390,10,50,25],'Callback',{@submit_button_Callback});
+
+
+    function filepath_Callback(source,eventdata)
+        dropSet_filepath = [uigetdir(pwd), '\'];
+    end
+
+    function headerlines_Callback(source,eventdata)
+        dropSet_headerlines = 0; %get(source, 'Value');
+    end
+
+    function submit_button_Callback(source,eventdata)
+%         index_selected = get(filelist, 'Value');
+%         filelist_all = get(filelist, 'String');
+%         filelist_selected = filelist_all(index_selected); %this probably won't work
+        test = dropSet('test', dropSet_filepath, dropSet_headerlines, true)
+    end
+
+    dropSet_filepath = [];
+    dropSet_headerlines = 0;
+
+    set(f, 'Visible', 'on');
+end

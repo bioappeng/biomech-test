af0cc8eb182dc5b7ea097b49fc0396658494d463
John Peterson <sn0risn@gmail.com>
2013-07-07 17:18:49 +0200

    made test_processing mocks a bit more tddish

--- test_processing.m
+++ test_processing.m
@@ -17,22 +17,15 @@ classdef test_processing < matlab.unittest.TestCase
             testCase.proc = processor();
         end
 
-        function setupSetMock(testCase)
+        function setupMocks(testCase)
             testCase.Set.num_drops = 3;
-            drop1.Value.accx = [1,2,3,4];
-            drop2.Value.accx = [1,2,500,-501];
-            drop3.Value.accx = [-100,2,3];
-            drop1.Value.accy = [1,2,3,4];
-            drop2.Value.accy = [1,2,500,-501];
-            drop3.Value.accy = [-100,2,3];
-            drop1.Value.accz = [1,2,3,4];
-            drop2.Value.accz = [1,2,500,-501];
-            drop3.Value.accz = [-100,2,3];
-            testCase.Set.drops = [drop1, drop2, drop3];
         end
     end
 
     methods(TestMethodTeardown)
+        function teardown(testCase)
+            clear testCase.Set;
+        end
     end
 
     methods(Test)
@@ -49,18 +42,30 @@ classdef test_processing < matlab.unittest.TestCase
         end
 
         function test_max_accx(testCase)
+            drop1.Value.accx = [1,2,3,4];
+            drop2.Value.accx = [1,2,500,-501];
+            drop3.Value.accx = [-100,2,3];
+            testCase.Set.drops = [drop1, drop2, drop3];
             testCase.proc.apply_process(testCase.collector,...
                                         testCase.Set, @max_accx);
             testCase.assertEqual(testCase.collector.calculated.max_accx, [4;501;100]);
         end
 
         function test_max_accy(testCase)
+            drop1.Value.accy = [1,2,3,4];
+            drop2.Value.accy = [1,2,500,-501];
+            drop3.Value.accy = [-100,2,3];
+            testCase.Set.drops = [drop1, drop2, drop3];
             testCase.proc.apply_process(testCase.collector,...
                                         testCase.Set, @max_accy);
             testCase.assertEqual(testCase.collector.calculated.max_accy, [4;501;100]);
         end
 
         function test_max_accz(testCase)
+            drop1.Value.accz = [1,2,3,4];
+            drop2.Value.accz = [1,2,500,-501];
+            drop3.Value.accz = [-100,2,3];
+            testCase.Set.drops = [drop1, drop2, drop3];
             testCase.proc.apply_process(testCase.collector,...
                                         testCase.Set, @max_accz);
             testCase.assertEqual(testCase.collector.calculated.max_accz, [4;501;100]);

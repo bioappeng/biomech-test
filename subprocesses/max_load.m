d14d84efd978eebaeb71fd29d4c954198119e5b8
John Peterson <sn0risn@gmail.com>
2013-07-17 16:20:13 +0100

    deleted subprocess that don't work

--- subprocesses/max_load.m
+++
@@ -1,8 +0,0 @@
-%find drop maxima of sensor data
-function max_load(collector, Set, p)
-    field_number = collector.add_field('max_load');
-    for i=1:Set.num_drops
-        drop = Set.drops(i).Value;
-        collector.add_value(max(abs(drop.load)), field_number);
-    end
-end

ed1e5bfd69598a85157b525457d209846e053888
John Peterson <sn0risn@gmail.com>
2013-08-08 15:26:56 +0100

    removed preallocation of array in max --not needed

--- lib/framework/subprocesses/process_max_accz.m
+++ lib/framework/subprocesses/process_max_accz.m
@@ -1,7 +1,6 @@
 classdef process_max_accz < process & handle
     methods (Static)
         function run(collector, Set)
-            max_acc = zeros(Set.num_drops, 1);
             for i=1:Set.num_drops
                 drop = Set.drops(i).Value;
                 max_acc(i, 1) = max(abs(drop.accz.data));

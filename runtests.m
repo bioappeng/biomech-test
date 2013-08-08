3a660d0b63a69271ce2794202b598e1c60c83f02
John Peterson <sn0risn@gmail.com>
2013-08-08 22:09:09 +0100

    added runtests for better testing reporting/run

---
+++ runtests.m
@@ -0,0 +1,16 @@
+function results = runtests(which_tests)
+    import matlab.unittest.TestSuite
+
+    switch which_tests
+        case 'all',
+            results = run(TestSuite.fromFolder('test'));
+    end
+
+    time = 0.0;
+    for i=1:length(results)
+        time = time + results(i).Duration;
+    end
+    num_passed = nnz([results.Passed]);
+    disp([num2str(num_passed), ' passed & ', num2str(numel(results) - num_passed), ' failed in ', num2str(time), 's'])
+    disp(' ')
+end

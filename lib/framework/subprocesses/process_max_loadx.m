7782e52a869c0faa94204c0ae37e039564f79138
John Peterson <sn0risn@gmail.com>
2013-12-04 15:13:50 -0500

    loadx,y,z now windowed with accelerations

--- lib/framework/subprocesses/process_max_loadx.m
+++ lib/framework/subprocesses/process_max_loadx.m
@@ -21,7 +21,7 @@ classdef process_max_loadx < handle
         function run(collector, Set)
             for i=1:Set.num_drops
                 drop = Set.get_drop(i);
-                max_value(i, 1) = max((drop.signals('loadx').data));
+                max_value(i, 1) = max((drop.signals('loadx').data(drop.window_start:drop.window_end)));
             end
             collector.add_field(max_value, 'max_loadx');
         end

9a468eadde0b71b8e58a4d97ed9ab29ec05f5b86
John Peterson <sn0risn@gmail.com>
2013-12-04 13:49:41 -0500

    fixed paths in main.m to reflect directory change

--- gui/main.m
+++ gui/main.m
@@ -1,6 +1,6 @@
-addpath('lib/framework/');
-addpath('lib/resources/');
-addpath('lib/resources/yamlmatlab/');
-addpath('lib/framework/subprocesses');
+addpath('../lib/framework/');
+addpath('../lib/resources/');
+addpath('../lib/resources/yamlmatlab/');
+addpath('../lib/framework/subprocesses');
 
 load_data()

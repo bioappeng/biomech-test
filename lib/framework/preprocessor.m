ab1ad531e8341a86bb89c7ee58beca6a64f191d4
John Peterson <sn0risn@gmail.com>
2013-12-05 12:26:07 -0500

    removed offset in setting file
    those should not be hardcoded values

--- lib/framework/preprocessor.m
+++ lib/framework/preprocessor.m
@@ -40,47 +40,39 @@ classdef preprocessor < handle
         end
 
         function calibrate_position(obj, pot)
-            pot.data = pot.data * obj.set.settings.settings.string_pot_slope + ...
-                    obj.set.settings.settings.string_pot_offset;
-%             pos_min = min(position);
-%             pot.data = position - pos_min;
+            pot.data = pot.data * obj.set.settings.settings.string_pot_calibration;
+%            pos_min = min(position);
+%            pot.data = position - pos_min;
         end
 
         function calibrate_single_axis_load(obj, load)
-            load.data = load.data * obj.set.settings.settings.uni_load_slope + ...
-                    obj.set.settings.settings.uni_load_offset;
+            load.data = load.data * obj.set.settings.settings.uni_load_calibration;
         end
 
         function calibrate_acc_x(obj, acc)
-            acc.data = acc.data * obj.set.settings.settings.acc_x_slope + ...
-                obj.set.settings.settings.acc_x_offset;
+            acc.data = acc.data * obj.set.settings.settings.acc_x_calibration;
         end
 
         
         function calibrate_acc_y(obj, acc)
-            acc.data = acc.data * obj.set.settings.settings.acc_y_slope + ...
-                obj.set.settings.settings.acc_y_offset;
+            acc.data = acc.data * obj.set.settings.settings.acc_y_calibration;
         end
         
         function calibrate_acc_z(obj, acc)
-            acc.data = acc.data * obj.set.settings.settings.acc_z_slope + ...
-                obj.set.settings.settings.acc_z_offset;
+            acc.data = acc.data * obj.set.settings.settings.acc_z_calibration;
         end
         
 
         function calibrate_load_x(obj, load)
-            load.data = load.data * obj.set.settings.settings.load_x_slope+...
-                    obj.set.settings.settings.load_x_offset;
+            load.data = load.data * obj.set.settings.settings.load_x_calibration;
         end
         
         function calibrate_load_y(obj, load)
-            load.data = load.data * obj.set.settings.settings.load_y_slope+...
-                    obj.set.settings.settings.load_y_offset;
+            load.data = load.data * obj.set.settings.settings.load_y_calibration;
         end
         
         function calibrate_load_z(obj, load)
-            load.data = load.data * obj.set.settings.settings.load_z_slope+...
-                    obj.set.settings.settings.load_z_offset;
+            load.data = load.data * obj.set.settings.settings.load_z_calibration;
         end
     end
 end

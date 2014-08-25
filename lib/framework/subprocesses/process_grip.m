babae8ea0ece7341fa09e5c97a84b03fc8433ea0
John Peterson <sn0risn@gmail.com>
2014-08-25 16:08:50 -0400

    fixed failing to_run in grip

--- lib/framework/subprocesses/process_grip.m
+++ lib/framework/subprocesses/process_grip.m
@@ -11,7 +11,7 @@ classdef process_grip < handle
 
     methods (Static)
         function to_run = assess_to_run(set)
-            first_drop_signals = set.get_drop(1)
+            first_drop_signals = set.get_drop(1).signals
             if first_drop_signals('loadz').data == true...
                 and first_drop_signals('loady').data == true...
                 and first_drop_signals('pot').data == true;
@@ -24,6 +24,9 @@ classdef process_grip < handle
         function run(collector, set)
             for i=1:set.num_drops
                 drop = set.get_drop(i);
+                max_value(i, 1) = max((drop.signals('loadz').data(drop.window_start:drop.window_end)));
+            end
+            collector.add_field(max_value, 'max_loadz');
         end
     end
 end
--- lib/framework/subprocesses/process_responsiveness.m
+++ lib/framework/subprocesses/process_responsiveness.m
@@ -11,12 +11,15 @@ classdef process_responsiveness < handle
 
     methods (Static)
         function to_run = assess_to_run(set)
+            to_run = true
         end
 
         function run(collector, set)
             for i=1:set.num_drops
                 drop = set.get_drop(i);
+                max_value(i, 1) = max((drop.signals('loadz').data(drop.window_start:drop.window_end)));
             end
+            collector.add_field(max_value, 'max_loadz');
         end
     end
 end
